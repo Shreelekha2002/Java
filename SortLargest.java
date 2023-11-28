8c8935917488fa58b366764202cb4d3cbd77bc69
Shreelekha2002 <shreelekha.xworkz@gmail.com>
2023-11-28 11:53:51 +0530

    new files

---
+++ SortLargest.java
@@ -0,0 +1,7 @@
+public class SortLargest {    
+    public static void main(String[] args) {        
+        //Initialize array     
+        int a[] = {5, 2, 8, 7, 1}; 
+		System.out.println("Largest element is:"+a[a.length-3]);
+	}
+}
